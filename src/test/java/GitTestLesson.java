4c4f28324db448ca2e59107add75420f6e98de24
vladislavpopov <vd.popov7@gmail.com>
2022-08-05 19:29:22 +0300

    Поправил количество треков.

--- src/test/java/GitTestLesson.java
+++ src/test/java/GitTestLesson.java
@@ -4,7 +4,7 @@ public class GitTestLesson {
 
     void GitTest (){
 
-        int trackCount = 6;
+        int trackCount = 7; // Ты не видишь? Треков всего 7!
 
         String track1 = "Intro";
         String track2 = "Ария Змея";
