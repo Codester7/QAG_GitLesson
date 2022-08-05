0239c7ae9a9213cbdcd856d3b3bca2f557a41282
vladislavpopov <vd.popov7@gmail.com>
2022-08-05 19:08:38 +0300

    first commit

---
+++ src/test/java/GitTestLesson.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class GitTestLesson {
+
+    void GitTest (){
+
+        int trackCount = 7;
+
+        String track1 = "Intro";
+        String track2 = "Ария Змея";
+        String track3 = "Ария Зайца";
+        String track4 = "Ария Лося";
+        String track5 = "Аутро";
+    }
+}
